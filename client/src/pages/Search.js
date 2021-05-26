666e4548dace56a26a9c30fbc4829c7007842a1c
Erick Meline <erickmeline@gmail.com>
2021-05-26 15:29:05 -0700

    fix no results display

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -5,16 +5,13 @@ import Results from "../components/Results"
 const Search = () => {
   const inputEl = useRef('');
   const [searchResults, setsearchResults] = useState([]);
-  const [searchTerm, setsearchTerm] = useState('');
-
-  const handleChange = (e) => {
-    setsearchTerm(e.target.value);
-  }
+  const [searched, setsearched] = useState(false);
 
   const handleSearch = (e) => {
     e.preventDefault();
     API.getGoogle(inputEl.current.value).then((res) => {
       setsearchResults(res);
+      setsearched(true);
     });
   }
 
@@ -28,13 +25,13 @@ const Search = () => {
     <div className="container">
       <form className="form-group">
         <h2>Search for and save Books of Interest</h2>
-        <input ref={inputEl} className="form-control" type="text" onChange={(e) => {handleChange(e)}} placeholder="Search a Book" />
+        <input ref={inputEl} className="form-control" type="text" placeholder="Search a Book" />
         <button className="btn btn-dark mt-3 mb-5" onClick={(e) => handleSearch(e)}>Search</button>
       </form>
       {
-        !searchResults.length && searchTerm.length ?
+        !searchResults.length && searched ?
         <h3>No Results</h3> :
-        searchResults.length && searchTerm.length ?
+        searchResults.length && searched ?
         <div><h3>Results</h3><ul>{searchResults.map((book) => <Results key={book._id} book={book} handleSave={handleSave} />)}</ul></div> :
         null
       }
